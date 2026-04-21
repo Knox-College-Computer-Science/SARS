6fa384a5298128b7ffc1c25ae662af8a8abf7cf9
Sandesh-dhakal74 <128986457+Sandesh-dhakal74@users.noreply.github.com>
2026-04-21 11:42:11 -0500

    added frontend as regular files instead of submodule

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,15 @@
+import Sidebar from "./components/Sidebar";
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="flex h-screen bg-[#343541] text-white">
+        <Sidebar />
+        <main className="flex-1 flex flex-col">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
