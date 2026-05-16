305da9546664bced3c4bf89e391c7f706af27f38
Sandesh-dhakal74 <128986457+Sandesh-dhakal74@users.noreply.github.com>
2026-05-16 17:44:43 -0500

    Final Fixed

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,10 @@
-import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: path.resolve(__dirname),
-  },
   allowedDevOrigins: ["10.7.5.80"],
+  experimental: {
+    turbopackMemoryLimit: 512,
+  },
 };
 
 export default nextConfig;
